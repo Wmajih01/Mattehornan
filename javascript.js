e95fef55bfc70d7276a86d4b53442250458cf7b4
unknown <alexander.bykadorov@skola.botkyrka.se>
2020-09-28 14:11:33 +0200

    first commit

---
+++ javascript.js
@@ -0,0 +1,21 @@
+const button2 = document.getElementById("button1")
+
+button2.onclick = startquiz
+
+let num1 = Math.floor(Math.random() * 10)
+let num2 = Math.floor(Math.random() * 10)
+
+function startquiz() {
+    const x = num1 + num2
+    const y = prompt("Vad är " + num1 + " + " + num2 + " ?")
+    if (y == x) {
+        alert("Ja det är rätt")
+        num1 = Math.floor(Math.random() * 10)
+        num2 = Math.floor(Math.random() * 10)
+    }
+    else {
+        alert("Fel svaret är " + x)
+        num1 = Math.floor(Math.random() * 10)
+        num2 = Math.floor(Math.random() * 10)
+    }
+}
